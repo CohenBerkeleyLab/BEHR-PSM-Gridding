8bfdf835603f4db03aaadf90f4d3e7e82bb73819
Josh Laughner <joshlaugh5@gmail.com>
2016-09-15 15:31:07 -0700

    BEHRDaily_Map.py now conforms to PEP8 better, grid name changed to "northamerica_behr" and should be 1201x501 now

--- BEHRDaily_Map.py
+++ BEHRDaily_Map.py
@@ -7,10 +7,10 @@ import numpy as np
 import numpy.ma as ma
 
 import glob
-#import sys
-#sys.path.append('/usr/users/annette.schuett/Masterarbeit/omi-master/omi')
+# import sys
+# sys.path.append('/usr/users/annette.schuett/Masterarbeit/omi-master/omi')
 import omi
-#import '/usr/users/annette.schuett/Masterarbeit/omi-master/omi'
+# import '/usr/users/annette.schuett/Masterarbeit/omi-master/omi'
 import glob
 from datetime import date
 import calendar
@@ -59,32 +59,31 @@ omi.he5.create_name2dataset(
     NAME2DATASET_NO2
 )
 List = ['TiledArea', 'TiledCornerLatitude', 'TiledCornerLongitude',
-     'FoV75Area', 'FoV75CornerLatitude', 'FoV75CornerLongitude',
-     'Latitude', 'Longitude', 'SpacecraftAltitude',
-     'SpacecraftLatitude', 'SpacecraftLongitude',
-     'CloudRadianceFraction', 'CloudPressure', 'BEHRColumnAmountNO2Trop',
-     'ColumnAmountNO2TropStd', 'RootMeanSquareErrorOfFit',
-     'vcdQualityFlags', 'XTrackQualityFlags',
-     'SolarZenithAngle', 'Time']
+        'FoV75Area', 'FoV75CornerLatitude', 'FoV75CornerLongitude',
+        'Latitude', 'Longitude', 'SpacecraftAltitude',
+        'SpacecraftLatitude', 'SpacecraftLongitude',
+        'CloudRadianceFraction', 'CloudPressure', 'BEHRColumnAmountNO2Trop',
+        'ColumnAmountNO2TropStd', 'RootMeanSquareErrorOfFit',
+        'vcdQualityFlags', 'XTrackQualityFlags',
+        'SolarZenithAngle', 'Time']
 
 
 def readin(Filename):
-    f = h5py.File(Filename,'r')
+    f = h5py.File(Filename, 'r')
     dictionary = {}
-    #for name in f:
-         #str = 'array'
-         #str2 = str+'_%s' %np.str(name) # = np.get(name)
-         #print str2
-         #dictionary[str2] =  f.get(name)
-         #print name
-    return f  
+    # for name in f:
+    # str = 'array'
+    # str2 = str+'_%s' %np.str(name) # = np.get(name)
+    # print str2
+    # dictionary[str2] =  f.get(name)
+    # print name
+    return f
 
 
 def preprocessing(gridding_method, Time, BEHRColumnAmountNO2Trop,
-    ColumnAmountNO2TropStd, FoV75Area, CloudRadianceFraction,
-    RootMeanSquareErrorOfFit, SolarZenithAngle, vcdQualityFlags,
-    XTrackQualityFlags, **kwargs):
-
+                  ColumnAmountNO2TropStd, FoV75Area, CloudRadianceFraction,
+                  RootMeanSquareErrorOfFit, SolarZenithAngle, vcdQualityFlags,
+                  XTrackQualityFlags, **kwargs):
     # mask of bad values
     mask = BEHRColumnAmountNO2Trop.mask | ColumnAmountNO2TropStd.mask
 
@@ -107,24 +106,22 @@ def preprocessing(gridding_method, Time, BEHRColumnAmountNO2Trop,
     area = FoV75Area.reshape(1, FoV75Area.size)
     area = area.repeat(BEHRColumnAmountNO2Trop.shape[0], axis=0)
 
-    
     weights = ma.array(1.0 / area, mask=mask)
-    
 
     return values, errors, stddev, weights
 
 
 def main(year, month, day, gridding_method, grid_name, List):
-
     # 1. Define a grid
     # (a) by giving lower-left and upper-right corner
-    grid_name = "northamerica_josh"
-    #grid = omi.Grid(llcrnrlat=40.0, urcrnrlat=55.0,llcrnrlon=-5.0, urcrnrlon=20.0, resolution=0.002); grid_name = 'Germany'#7500*12500
-    #grid = omi.Grid(llcrnrlat= 17.8 , urcrnrlat=53.6 ,llcrnrlon=96.9 , urcrnrlon= 106.8, resolution=0.01); #grid_name = 'Northamerica'#6000*4000
-    grid = omi.Grid.by_name(grid_name)
+
+    # grid = omi.Grid(llcrnrlat=40.0, urcrnrlat=55.0,llcrnrlon=-5.0, urcrnrlon=20.0, resolution=0.002); grid_name = 'Germany'#7500*12500
+    # grid = omi.Grid(llcrnrlat= 17.8 , urcrnrlat=53.6 ,llcrnrlon=96.9 , urcrnrlon= 106.8, resolution=0.01); #grid_name = 'Northamerica'#6000*4000
+
     # (b) or by reading this data from a JSON file
     #    (the default file can be found in omi/data/gridds.json)
-   # grid = omi.Grid.by_name(grid_name)
+    grid_name = "northamerica_behr"
+    grid = omi.Grid.by_name(grid_name)
 
     # 2. Define parameter for PSM
     #    - gamma (smoothing parameter)
@@ -139,11 +136,12 @@ def main(year, month, day, gridding_method, grid_name, List):
     #    >>> omi.he5.create_name2dataset(path, list_of_dataset_names, dict)
     #    can be helpful (see above).
     name2datasets = [NAME2DATASET_NO2, NAME2DATASET_PIXEL]
-    
-    filename = '/usr/users/annette.schuett/Masterarbeit/Vergleich_Ber/HDF/BEHR-PSM/OMI_BEHR_v2-1B_%s%s%s.hdf'%(year, month, day)
+
+    filename = '/usr/users/annette.schuett/Masterarbeit/Vergleich_Ber/HDF/BEHR-PSM/OMI_BEHR_v2-1B_%s%s%s.hdf' % (
+        year, month, day)
     print filename
-    
-    #f = readin(filename)  
+
+    # f = readin(filename)
     f = h5py.File(filename, 'r')
     data = dict()
     """
@@ -158,29 +156,30 @@ def main(year, month, day, gridding_method, grid_name, List):
                 data[name] = ma.array(value)
                 
     """
-    
+
     for orbit in f['Data']:
-        #for name in f['Data'][orbit]:
+        # for name in f['Data'][orbit]:
         for i in range(len(List)):
             name = List[i]
-            #print name,':'
-            field = f.get(('Data/%s/%s')%(orbit,name), None)
+            # print name,':'
+            field = f.get(('Data/%s/%s') % (orbit, name), None)
             if field is None:
-                print name, 'Warning'#: No data of name.' % (name)
+                print name, 'Warning'  #: No data of name.' % (name)
                 data[name] = None
-            else: 
-                #print '----------------------------------------<worked'
-                
-                #fill_value = field.attrs.get('_FillValue', None)
+            else:
+                # print '----------------------------------------<worked'
+
+                # fill_value = field.attrs.get('_FillValue', None)
                 fill_value = field.fillvalue
                 print name, fill_value
-                #missing_value = field.attrs.get('MissingValue', None)
+                # missing_value = field.attrs.get('MissingValue', None)
                 missing_value = fill_value
                 scale = field.attrs.get('ScaleFactor', 1.0)
                 offset = field.attrs.get('Offset', 0.0)
 
                 if fill_value is None and missing_value is None:
-                    raise ValueError('Missing `_FillValue` or `MissingValue` at orbit %s wirth product %s ' % (orbit, name))
+                    raise ValueError(
+                        'Missing `_FillValue` or `MissingValue` at orbit %s wirth product %s ' % (orbit, name))
 
                 value = field.value
                 mask = (field.value == fill_value) | (field.value == missing_value)
@@ -198,22 +197,18 @@ def main(year, month, day, gridding_method, grid_name, List):
                     data[name] = ma.array(value, mask=mask, dtype=np.float64)
                 else:
                     data[name] = ma.array(value, mask=mask, dtype=value.dtype)
-                if len(np.shape(data[name]))==2:
+                if len(np.shape(data[name])) == 2:
                     data[name] = data[name].T
-                
-                    
-                print name, np.shape(data[name])    
-                    
-                    
-                                
+
+                print name, np.shape(data[name])
+
         ##data['TiledCornerLongitude'] = np.transpose(data['TiledCornerLongitude'], axes =(1,0,2))
         ##data['TiledCornerLatitude'] = np.transpose(data['TiledCornerLatitude'], axes =(1,0,2))
-        data['TiledCornerLongitude'] = np.transpose(data['TiledCornerLongitude'], axes =(2,1,0))
-        data['TiledCornerLatitude'] = np.transpose(data['TiledCornerLatitude'], axes =(2,1,0))
-        data['FoV75CornerLatitude'] = np.transpose(data['FoV75CornerLatitude'], axes =(2,1,0))
-        data['FoV75CornerLongitude'] = np.transpose(data['FoV75CornerLongitude'], axes =(2,1,0))
-        
-        
+        data['TiledCornerLongitude'] = np.transpose(data['TiledCornerLongitude'], axes=(2, 1, 0))
+        data['TiledCornerLatitude'] = np.transpose(data['TiledCornerLatitude'], axes=(2, 1, 0))
+        data['FoV75CornerLatitude'] = np.transpose(data['FoV75CornerLatitude'], axes=(2, 1, 0))
+        data['FoV75CornerLongitude'] = np.transpose(data['FoV75CornerLongitude'], axes=(2, 1, 0))
+
         data['TiledArea'] = data['TiledArea'].T[0]
         data['FoV75Area'] = data['FoV75Area'].T[0]
         data['SpacecraftAltitude'] = data['SpacecraftAltitude'].T[0]
@@ -222,26 +217,21 @@ def main(year, month, day, gridding_method, grid_name, List):
         data['Time'] = data['Time'].T[0]
         print np.shape(data['TiledCornerLongitude']), np.shape(data['FoV75Area'])
         for i in range(len(List)):
-            print List[i],':', np.shape(data[List[i]])
-            
-            
-            
-        print ''    
-        
-        
+            print List[i], ':', np.shape(data[List[i]])
+
+        print ''
+
         # 5) Check for missing corner coordinates, i.e. the zoom product,
         #    which is currently not supported
         if (data['TiledCornerLongitude'].mask.any() or
-            data['TiledCornerLatitude'].mask.any()
-        ):
+                data['TiledCornerLatitude'].mask.any()):
             continue
 
         # 6) Clip orbit to grid domain
         lon = data['FoV75CornerLongitude']
         lat = data['FoV75CornerLatitude']
-        #pdb.set_trace()
-        data = omi.clip_orbit(grid, lon, lat, data, boundary=(2,2))
-    
+        # pdb.set_trace()
+        data = omi.clip_orbit(grid, lon, lat, data, boundary=(2, 2))
 
         if data['BEHRColumnAmountNO2Trop'].size == 0:
             continue
@@ -259,23 +249,21 @@ def main(year, month, day, gridding_method, grid_name, List):
         if np.all(values.mask):
             continue
 
-
         # 8) Grid orbit using PSM or CVM:
-        #print 'time: %s, orbit: %d' % (timestamp, orbit)
+        # print 'time: %s, orbit: %d' % (timestamp, orbit)
         if gridding_method == 'psm':
             grid = omi.psm_grid(grid,
-                data['Longitude'], data['Latitude'],
-                data['TiledCornerLongitude'], data['TiledCornerLatitude'],
-                values, errors, stddev, weights, missing_values,
-                data['SpacecraftLongitude'], data['SpacecraftLatitude'],
-                data['SpacecraftAltitude'],
-                gamma[data['ColumnIndices']],
-                rho_est
-            )
+                                data['Longitude'], data['Latitude'],
+                                data['TiledCornerLongitude'], data['TiledCornerLatitude'],
+                                values, errors, stddev, weights, missing_values,
+                                data['SpacecraftLongitude'], data['SpacecraftLatitude'],
+                                data['SpacecraftAltitude'],
+                                gamma[data['ColumnIndices']],
+                                rho_est
+                                )
         else:
             grid = omi.cvm_grid(grid, data['FoV75CornerLongitude'], data['FoV75CornerLatitude'],
-            values, errors, weights, missing_values)
-
+                                values, errors, weights, missing_values)
 
     # 9) The distribution of values and errors has to be normalised
     #    with the weight.
@@ -283,42 +271,34 @@ def main(year, month, day, gridding_method, grid_name, List):
 
     # 10) The Level 3 product can be saved as HDF5 file
     #     or converted to an image (requires matplotlib and basemap
-    grid.save_as_he5('%s_%s_%s_%s_%s.he5' % (grid_name,year,  month,  day, gridding_method))
-    grid.save_as_image('%s_%s_%s_%s_%s.png' % (grid_name, year,  month,  day, gridding_method), vmin=0, vmax=rho_est)
-    #grid.save_as_image('%s_%s_%s_%s_%s.he5' % ( str(start_date)[8:10],  str(start_date)[5:7],  str(start_date)[0:4], grid_name, gridding_method), vmin=0, vmax=rho_est)
+    grid.save_as_he5('%s_%s_%s_%s_%s.he5' % (grid_name, year, month, day, gridding_method))
+    grid.save_as_image('%s_%s_%s_%s_%s.png' % (grid_name, year, month, day, gridding_method), vmin=0, vmax=rho_est)
+    # grid.save_as_image('%s_%s_%s_%s_%s.he5' % ( str(start_date)[8:10],  str(start_date)[5:7],  str(start_date)[0:4], grid_name, gridding_method), vmin=0, vmax=rho_est)
 
     # 11) It is possible to set values, errors and weights to zero.
     grid.zero()
-    
-
 
 
 if __name__ == '__main__':
 
-
     grid_name = 'NA'
 
-    
-    
     files = glob.glob('/usr/users/annette.schuett/Masterarbeit/Vergleich_Ber/HDF/BEHR-PSM/OMI_BEHR_v2-1B_2015*.hdf')
     np.sort(files)
     counter = 0
-    
-    
+
     for filename in files:
-        
         year = filename[82:86]
         month = filename[86:88]
         day = filename[88:90]
         main(year, month, day, 'psm', grid_name, List)
-        counter +=1
-        #if counter == 2:
-            #break
-        #print year, month, day   
-    
-    
-        #start_date = datetime(year,month,day)
-        #end_date = datetime(year,month,day+1)
+        counter += 1
+        # if counter == 2:
+        # break
+        # print year, month, day
+
+        # start_date = datetime(year,month,day)
+        # end_date = datetime(year,month,day+1)
     """
     print "start_date =", start_date 
 
@@ -327,11 +307,4 @@ if __name__ == '__main__':
    
     print "First Algorithm ready"
      """
-        #main(year, month, day, 'psm', grid_name, data_path)
-  
-
-
-
-
-
-
+    # main(year, month, day, 'psm', grid_name, data_p
